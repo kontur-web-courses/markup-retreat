0ea843f7194f2545fe6c087d9ebd0902f059bac9
TashK101 <Natalia.Karimova@urfu.me>
2023-04-10 15:37:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let rating = document.getElementsByClassName("rating_star")
+
+
+rating[0].addEventListener('mouseover', )
+
+
+for (let i = 0; i< 5; i++){
+    rating[i].addEventListener('mouseover', e => 
+    {
+        console.log(1)
+        rating[i].color = '#000000B2';
+    })
+
+    rating[i].addEventListener('mouseleave', e => {
+        star1.color = '#dabd18b2';
+    })
+
+    rating[i].addEventListener('mousedown', e => {
+        star1.src = star1.color = '#000000B2';
+        for (let i = 0; i<5; i++){
+            rating[i].removeEventListener('mouseover');
+            rating[i].removeEventListener('mouseleave');
+        }
+    })
+}
